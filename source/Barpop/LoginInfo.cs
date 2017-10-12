75bd116c1476b9f037aed87163ca77acf09c9dcb
thisguyislegit <31456927+thisguyislegit@users.noreply.github.com>
2017-10-12 14:38:20 -0400

    Add files via upload

---
+++ source/Barpop/LoginInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barpop
+{
+    class LoginInfo
+    {
+     
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public LoginInfo(string firstName, string middleName, string lastName, DateTime dateTime)
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+            DateTime = dateTime;
+        }
+    }
+        
+    
+}
+
